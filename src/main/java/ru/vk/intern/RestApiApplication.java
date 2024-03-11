4a32cb4c2bf288dca16dd394f057899fb0f4ed2a
Ksenia Solnyshko <frezzerrro@gmail.com>
2024-03-12 01:25:23 +0300

    Add api

---
+++ src/main/java/ru/vk/intern/RestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.vk.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class RestApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiApplication.class, args);
+    }
+}
